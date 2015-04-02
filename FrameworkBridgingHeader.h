1ae301d961d0c52b76db623e4439a8ec1b5f7402
agoddijn <alex.goddijn@gmail.com>
2015-04-02 12:30:37 -0700

    Added firebase framework. Loading Cloudinary specifically

---
+++ FrameworkBridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  FrameworkBridgingHeader.h
+//  ZenFlow3
+//
+//  Created by Alexander Goddijn on 20/03/2015.
+//  Copyright (c) 2015 Alexander Goddijn. All rights reserved.
+//
+
+#ifndef ZenFlow3_FrameworkBridgingHeader_h
+#define ZenFlow3_FrameworkBridgingHeader_h
+#import "CloudinaryFactory.h"
+#import "Firebase/Firebase.h"
+
+#endif
